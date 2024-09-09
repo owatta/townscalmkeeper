1790636530a254c3ab1da4a9e2b8d6ecac79439b
Konstantin-Maca <konstantinmaca@gmail.com>
2024-09-10 02:37:57 +0500

    Re-initial commit.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,86 @@
-use bevy::{ prelude::*, sprite::Wireframe2dPlugin };
+use bevy::prelude::*;
 
-use systems::*;
-use plugins::*;
+const GRID_SIZE: (usize, usize) = (30, 30);
+const TILE_WIDTH: usize = 32;
 
-mod resources;
-mod components;
-mod systems;
-mod plugins;
+enum Tile {
+    SmallHouse,
+    MediumHouse,
+    BigHouse,
+    Wiring,
+    Plumbing,
+    Road,
+    PowerStation,
+    WaterSource,
+    Empty,
+}
+
+#[derive(Component)]
+struct TileBundle {
+    kind: Tile,
+    sprite: SpriteBundle,
+}
+
+impl Tile {
+    fn sprite_path(&self) -> String {
+        match &self {
+            Tile::SmallHouse => "sprites/small_house.png".to_string(),
+            Tile::MediumHouse => "sprites/medium_house.png".to_string(),
+            _ => "blah".to_string(),
+        }
+    }
+}
+
+#[derive(Component)]
+struct Position(usize, usize);
+
+fn put_tile(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    kind: Tile,
+    pos: (usize, usize)
+) {
+    let sprite = SpriteBundle {
+        texture: asset_server.load(kind.sprite_path()),
+        ..default()
+    };
+    commands.spawn((Position(pos.0, pos.1), sprite));
+}
+
+fn update_tile_sprite_positions(mut tiles: Query<(&Position, &mut Transform)>) {
+    // determines each tile's position
+    // puts appropriate Transform value
+    for (pos, mut transform) in &mut tiles {
+        transform.translation.x = (pos.0 * TILE_WIDTH) as f32;
+        transform.translation.y = (pos.1 * TILE_WIDTH) as f32;
+    }
+}
+
+#[derive(Resource)]
+struct Wallet(i32);
+
+fn give_money(mut wallet: ResMut<Wallet>, tiles: Query<&TileBundle>) {
+    for tile in &tiles {
+        match tile.kind {
+            Tile::SmallHouse => {
+                wallet.0 += 10;
+            }
+            _ => (),
+        }
+    }
+}
 
 fn main() {
-    App::new()
-        .add_plugins((DefaultPlugins, Wireframe2dPlugin))
-        .add_plugins(ShapeConveyorPlugin)
-        .add_systems(Startup, setup)
-        .add_systems(Update, (
-            player_movement_system,
-        ))
-        .run();
+    let _app = App::new().add_plugins(DefaultPlugins).add_systems(Startup, setup).run();
 }
+
+fn setup(commands: Commands, asset_server: Res<AssetServer>) {
+    put_tile(commands, asset_server, Tile::SmallHouse, (3, 3));
+}
+
+// TODOs:
+// - make a house
+
+// unimportant:
+// - find a better way to handle Resources (stuff like `resource.0`
+// for value access)
